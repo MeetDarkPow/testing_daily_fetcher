4e8cbdc46f51ac004336d683a15bea6fe1cd0c00
Meet Bhatnagar <meet1708@gmail.com>
2021-03-22 11:34:32 +0530

    Update Test_1.R

--- Test_1.R
+++ Test_1.R
@@ -1,4 +1,4 @@
-# store API keys
+# storing API keys
 api_key <- "Vasln0HaAY8pkXu7J2xQkBX8c"
 api_secret_key <- "h5ra23jvrY8nq7vkUBld1mXXYwjsKR5iyNoH2CATUNKO1rdzq3"
 acc_token <- "1013338789580713984-2oRZj3Btzvgl0yZqJ8Wfr0GEmTzW34"
@@ -14,14 +14,17 @@ token <- create_token(
   access_secret = acc_secret
 )
 
-rtweet_data <- search_tweets(q="#rstats", since = Sys.Date()-2, 
-                             until = Sys.Date()-1, 
+# searching for required #rstats tweets
+rtweet_data <- search_tweets(q="#rstats", since = Sys.Date()-1, 
+                             until = Sys.Date(), 
                              retryonratelimit = TRUE, include_rts = FALSE)
 
+# creating a structured data frame
 rtweet_df <- data.frame(User_ID=rtweet_data$user_id, Status_ID=rtweet_data$status_id,
                         Tweet_Date=rtweet_data$created_at, Tweet_Text=rtweet_data$text, 
                         Retweet_Count=rtweet_data$retweet_count, Likes = rtweet_data$favorite_count)
 
+# saving #rstats tweets to a csv file
 write.table(rtweet_df, file = "data/rstats_Tweet.csv", sep = ",",
             row.names = FALSE, col.names = !file.exists("rstats_Tweet.csv"), 
             append = TRUE)
